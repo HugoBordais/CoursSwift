9e2245adc203669d005f01d59a76ed8795f57ae2
Hugo <hugo.bordais@ynov.com>
2019-05-15 19:19:29 +0200

    first commit

---
+++ Vinted_Course/Application/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Vinted_Course
+//
+//  Created by Hugo Bordais on 5/15/19.
+//  Copyright © 2019 Hugo Bordais. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    //    MARK: - public attribut
+    
+    var window: UIWindow?
+
+    //    MARK: - public methods
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
